c3e5203b502a4fb640ab4155578cb5d89ebc25cf
Maxime <max.ron@outlook.fr>
2023-10-23 11:29:54 +0200

    connexion to supabase working, add conexion file and edit tab 3 for testing

--- GABOR45_app/src/pages/Tab3.tsx
+++ GABOR45_app/src/pages/Tab3.tsx
@@ -1,25 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonCard, IonCardContent, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import { supabase } from '../supabaseClient';
+import React, { useEffect, useState } from 'react';
+
+async function fetchPhrase() {
+    const { data, error } = await supabase
+        .from('test')
+        .select('phrase')
+        .single();
+    if (error) {
+        console.error(error);
+        return;
+    }
+    return data?.phrase;
+}
+
 
 const Tab3: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
-  );
+    const [phrase, setPhrase] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function getPhrase() {
+            const fetchedPhrase = await fetchPhrase();
+            setPhrase(fetchedPhrase);
+        }
+        getPhrase();
+    }, []);
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 3</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 3</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonCard>
+                    <IonCardContent>
+                        {phrase}
+                    </IonCardContent>
+                </IonCard>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab3;
+
