e892fa2a1ae4318e01c6c7624db8ebcfb9f5efa0
andr3sEnrique <20213tn080@utez.edu.mx>
2023-10-24 00:17:02 +0200

    pull signInEmail

--- GABOR45_app/src/pages/Login.tsx
+++ GABOR45_app/src/pages/Login.tsx
@@ -1,12 +1,39 @@
 // src/pages/Login.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { supabase } from '../supabaseClient';
-import { IonPage, IonContent, IonButton } from '@ionic/react';
+import { IonButton, useIonToast, IonItem, IonLabel, IonInput, IonText, IonPage, IonContent } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
 
 const Login: React.FC = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [error, setError] = useState<string | null>(null);
+    const [showToast ] = useIonToast();
+
     const history = useHistory();
+
+    const handleLogin = async () => {
+        console.log("entro al login");
+        try {
+          const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+          });
+          console.log(data);
+    
+          if (error) {
+            setError(error.message);
+          } else {
+            await showToast({ message: 'Success', duration: 2000, color: 'success' });
+            // Inicio de sesión exitoso, puedes redirigir a otra página aquí
+            history.push('/profile');
+          }
+        } catch (error) {
+
+            setError("Hubo un error al iniciar sesión");
+        }
+    };
 
     const signInWithFacebook = async () => {
         const { data, error } = await supabase.auth.signInWithOAuth({
@@ -25,9 +52,31 @@ const Login: React.FC = () => {
     return (
         <IonPage>
             <IonContent>
-                <IonButton onClick={signInWithFacebook}>
+                <h2>Se connecter</h2>
+                <IonItem>
+                    <IonLabel position="floating">Email</IonLabel>
+                    <IonInput
+                    type="email"
+                    value={email}
+                    onIonChange={(e) => setEmail(e.detail.value || "")}
+                    />
+                </IonItem>
+                <IonItem>
+                    <IonLabel position="floating">Password</IonLabel>
+                    <IonInput
+                    type="password"
+                    value={password}
+                    onIonChange={(e) => setPassword(e.detail.value || "")}
+                    />
+                </IonItem>
+                {error && <IonText color="danger">{error}</IonText>}
+                <IonButton expand="full" onClick={handleLogin}>
+                    Se connecter
+                </IonButton>
+                <IonButton expand="full" onClick={signInWithFacebook} color="secondary">
                     Se connecter avec Facebook
                 </IonButton>
+
             </IonContent>
         </IonPage>
     );
