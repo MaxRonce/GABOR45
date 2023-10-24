e6eaf61e46465dc03abe0297c837538313df0167
andr3sEnrique <20213tn080@utez.edu.mx>
2023-10-24 10:12:49 +0200

    Login page with styles

--- GABOR45_app/src/pages/Login.tsx
+++ GABOR45_app/src/pages/Login.tsx
@@ -2,14 +2,19 @@
 
 import React, { useState } from 'react';
 import { supabase } from '../supabaseClient';
-import { IonButton, useIonToast, IonItem, IonLabel, IonInput, IonText, IonPage, IonContent } from '@ionic/react';
+import { IonButton, useIonToast, IonItem, IonLabel, IonInput, IonText, IonPage, IonContent,
+     IonImg, IonIcon, IonGrid, IonRow, IonCol, IonTabButton } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
+import logo from '../assets/logo_Gabor45.png';
+import mail from '../icons/mail.svg';
+import '../theme/custom.css';
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [error, setError] = useState<string | null>(null);
     const [showToast ] = useIonToast();
+    
 
     const history = useHistory();
 
@@ -52,23 +57,50 @@ const Login: React.FC = () => {
     return (
         <IonPage>
             <IonContent>
-                <h2>Se connecter</h2>
                 <IonItem>
-                    <IonLabel position="floating">Email</IonLabel>
-                    <IonInput
-                    type="email"
-                    value={email}
-                    onIonChange={(e) => setEmail(e.detail.value || "")}
-                    />
+                    <IonImg src={logo} alt="GABOR45" class="ion-img ion-margin ion-padding" />
                 </IonItem>
-                <IonItem>
-                    <IonLabel position="floating">Password</IonLabel>
-                    <IonInput
-                    type="password"
-                    value={password}
-                    onIonChange={(e) => setPassword(e.detail.value || "")}
-                    />
-                </IonItem>
+                <IonGrid class="ion-margin ion-padding">
+                    <IonRow>
+                        <IonCol size="1" class="ion-margin">
+                            <IonIcon src={mail} className="login-icon ion-icon" />
+                        </IonCol>
+                        <IonCol class='ion-margin-start'>
+                            <div className='login-input'>
+                                <IonInput
+                                label="Email"
+                                label-placement="floating"     
+                                type="email"
+                                placeholder='email@domain.com'
+                                value={email}
+                                onIonChange={(e) => setEmail(e.detail.value || "")}
+                                className='custom-input'
+                                />
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+
+                <IonGrid class="ion-margin-bottom ion-padding">
+                    <IonRow>
+                        <IonCol size="1" class="ion-margin">
+                            <IonIcon src={mail} className="login-icon ion-icon" />
+                        </IonCol>
+                        <IonCol class='ion-margin-start'>
+                            <div className='login-input'>
+                                <IonInput
+                                label="Mot de passe"
+                                label-placement="floating"     
+                                type="password"
+                                placeholder='********'
+                                value={password}
+                                onIonChange={(e) => setPassword(e.detail.value || "")}
+                                className='custom-input'
+                                />
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
                 {error && <IonText color="danger">{error}</IonText>}
                 <IonButton expand="full" onClick={handleLogin}>
                     Se connecter
