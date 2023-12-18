d5871508ac9e16b1f934ffdea8aa98356f7b3aaf
Noel-louis <louisnoel45000@gmail.com>
2023-12-18 16:49:26 +0100

    global file refactor

--- apps/GABOR45/src/pages/farmers/IndexFarmers.tsx
+++ apps/GABOR45/src/pages/farmers/IndexFarmers.tsx
@@ -1,128 +1,175 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router";
-import { IonContent, IonPage, IonSearchbar, IonToolbar, IonIcon, IonItem, IonGrid,
-   IonRow, IonCol, IonText, IonHeader } from '@ionic/react';
-import FarmerPage from './Farmers';
-import './Farmers.css';
-import arrowLeft from '../../icons/arrowLeft.svg';
-import search from '../../icons/search.svg';
-import FarmerCategories from './FarmerCategories';
-import FarmerSearchPage from './FarmersSearch';
-import FarmersByCategory from './FarmersByCategory';
+//React and React Router
+import React, { useEffect, useState } from "react";
+
+// Ionic Framework Components
+import {
+	IonContent,
+	IonPage,
+	IonSearchbar,
+	IonToolbar,
+	IonIcon,
+	IonItem,
+	IonGrid,
+	IonRow,
+	IonCol,
+	IonText,
+	IonHeader,
+} from "@ionic/react";
+
+// Custom Components and Styling
+import FarmerPage from "./Farmers";
+import "./Farmers.css";
+import arrowLeft from "../../icons/arrowLeft.svg";
+import search from "../../icons/search.svg";
+import FarmerCategories from "./FarmerCategories";
+import FarmerSearchPage from "./FarmersSearch";
+import FarmersByCategory from "./FarmersByCategory";
 
 const IndexFarmers: React.FC = () => {
-  const [page, setPage] = useState<string>("producteurs"); // State for the initial page [producteurs, produits
-  const [searchQuery, setSearchQuery] = useState<string>(""); // State for the search query
-  const [isActive, setIsActive] = useState([true, false]); 
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+	const [page, setPage] = useState<string>("producteurs"); // State for the initial page [producteurs, produits
+	const [searchQuery, setSearchQuery] = useState<string>(""); // State for the search query
+	const [isActive, setIsActive] = useState([true, false]);
+	const [selectedCategory, setSelectedCategory] = useState<string | null>(
+		null
+	);
 
-  useEffect(() => {
-    // reset the page when the component is loaded
-    const handleBeforeUnload = () => {  
-        setPage("producteurs");
-        setSearchQuery("");
-        setIsActive([true, false]);
-        setSelectedCategory(null);
-    };
-    window.addEventListener('focusout', handleBeforeUnload);
-  }, []);
-    //control the button active
-    const handleClick = (index:any) => {
-        setSelectedCategory(null);
-        const updateActive = [...isActive];
-        updateActive[index] = !updateActive[index];
-        for (let i = 0; i < updateActive.length; i++) {
-            if (i !== index) {
-                updateActive[i] = false;
-            }
-            
-        }
-        setIsActive(updateActive);
-        if(index === 1){
-            handleProduit();
-        } else {
-            handleNearby();
-        }
-      };
-    //handle the button produits
-    const handleProduit = () => {
-        const page = 'categories';
-        setPage(page);
-    }
-    //handle the button nearby
-    const handleNearby = () => {
-        const page = 'producteurs';
-        setPage(page);
-    }
+	useEffect(() => {
+		// reset the page when the component is loaded
+		const handleBeforeUnload = () => {
+			setPage("producteurs");
+			setSearchQuery("");
+			setIsActive([true, false]);
+			setSelectedCategory(null);
+		};
+		window.addEventListener("focusout", handleBeforeUnload);
+	}, []);
+	//control the button active
+	const handleClick = (index: any) => {
+		setSelectedCategory(null);
+		const updateActive = [...isActive];
+		updateActive[index] = !updateActive[index];
+		for (let i = 0; i < updateActive.length; i++) {
+			if (i !== index) {
+				updateActive[i] = false;
+			}
+		}
+		setIsActive(updateActive);
+		if (index === 1) {
+			handleProduit();
+		} else {
+			handleNearby();
+		}
+	};
+	//handle the button produits
+	const handleProduit = () => {
+		const page = "categories";
+		setPage(page);
+	};
+	//handle the button nearby
+	const handleNearby = () => {
+		const page = "producteurs";
+		setPage(page);
+	};
 
-    
-    const handleSearch = async (value:any) => {
-        if (value) {
-            const searchQuery = value.toLowerCase();
-            setSearchQuery(searchQuery);
-        }
-        else {
-            setSearchQuery("");
-        }
-    }
-    const handleBack = () => {
-        setSearchQuery("");
-        setPage("producteurs");
-        setSelectedCategory(null);
-    }
-    const showFarmersCategory =( category: string) => {
-        setSelectedCategory(category);
-        setPage("");
-        }
+	const handleSearch = async (value: any) => {
+		if (value) {
+			const searchQuery = value.toLowerCase();
+			setSearchQuery(searchQuery);
+		} else {
+			setSearchQuery("");
+		}
+	};
+	const handleBack = () => {
+		setSearchQuery("");
+		setPage("producteurs");
+		setSelectedCategory(null);
+	};
+	const showFarmersCategory = (category: string) => {
+		setSelectedCategory(category);
+		setPage("");
+	};
 
+	const navClass = (index: any) =>
+		`nav-line ${isActive[index] ? "active" : ""}`;
 
-      const navClass = (index:any) => `nav-line ${isActive[index] ? 'active' : ''}`;
-
-  return (
-    <IonPage>
-      <IonHeader>
-            <IonToolbar>
-                <IonItem lines="none" className="ion-margin-top">
-                    <IonIcon src={arrowLeft} onClick={handleBack} className='nav-icon ion-icon' />
-                    <IonSearchbar placeholder="SEARCH"
-                        value={searchQuery}
-                        onKeyDown={(e) => handleSearch(e.currentTarget.value)}
-                        className="ion-justify-content-end searchBar">
-                        <IonIcon src={search} slot="end" className="search-icon"/>
-                    </IonSearchbar>
-                    
-                </IonItem>
-                <IonGrid class="ion-margin-horizontal">
-                    <IonRow>
-                        <IonCol size="6" class="custom-center">
-                            <div className={navClass(0)} onClick={() => { handleClick(0)}}>
-                                <IonText className="nav-text ion-text-center">à proximité</IonText>
-                            </div>
-                        </IonCol>
-                        <IonCol size="6" class="custom-center">
-                            <div className={navClass(1)} onClick={() => {handleClick(1)}}>
-                                <IonText className="nav-text ion-text-center">Catégorie</IonText>
-                            </div>
-                        </IonCol>
-                    </IonRow>
-
-                </IonGrid> 
-            </IonToolbar>
-        </IonHeader>
-      <IonContent>
-        {searchQuery !== "" ? ( // if searchQuery is not empty, show the search page
-          <FarmerSearchPage searchQuery={searchQuery} page={page} />
-        ) : ( // else show the page selected
-          <>
-            {page === 'producteurs' ? <FarmerPage /> : null}
-            {page === 'categories' ? <FarmerCategories selectedCategory={selectedCategory}
-                onSelectCategory={(category) => showFarmersCategory(category)}/> : null}
-            {selectedCategory ? <FarmersByCategory categoryId={selectedCategory} /> : null}
-          </>
-        )}
-      </IonContent>
-    </IonPage>
-  );
-}
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonItem lines="none" className="ion-margin-top">
+						<IonIcon
+							src={arrowLeft}
+							onClick={handleBack}
+							className="nav-icon ion-icon"
+						/>
+						<IonSearchbar
+							placeholder="SEARCH"
+							value={searchQuery}
+							onKeyDown={(e) =>
+								handleSearch(e.currentTarget.value)
+							}
+							className="ion-justify-content-end searchBar"
+						>
+							<IonIcon
+								src={search}
+								slot="end"
+								className="search-icon"
+							/>
+						</IonSearchbar>
+					</IonItem>
+					<IonGrid class="ion-margin-horizontal">
+						<IonRow>
+							<IonCol size="6" class="custom-center">
+								<div
+									className={navClass(0)}
+									onClick={() => {
+										handleClick(0);
+									}}
+								>
+									<IonText className="nav-text ion-text-center">
+										à proximité
+									</IonText>
+								</div>
+							</IonCol>
+							<IonCol size="6" class="custom-center">
+								<div
+									className={navClass(1)}
+									onClick={() => {
+										handleClick(1);
+									}}
+								>
+									<IonText className="nav-text ion-text-center">
+										Catégorie
+									</IonText>
+								</div>
+							</IonCol>
+						</IonRow>
+					</IonGrid>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				{searchQuery !== "" ? ( // if searchQuery is not empty, show the search page
+					<FarmerSearchPage searchQuery={searchQuery} page={page} />
+				) : (
+					// else show the page selected
+					<>
+						{page === "producteurs" ? <FarmerPage /> : null}
+						{page === "categories" ? (
+							<FarmerCategories
+								selectedCategory={selectedCategory}
+								onSelectCategory={(category) =>
+									showFarmersCategory(category)
+								}
+							/>
+						) : null}
+						{selectedCategory ? (
+							<FarmersByCategory categoryId={selectedCategory} />
+						) : null}
+					</>
+				)}
+			</IonContent>
+		</IonPage>
+	);
+};
 
 export default IndexFarmers;
--- apps/GABOR45/src/pages/map/Map.tsx
+++ apps/GABOR45/src/pages/map/Map.tsx
@@ -1,34 +1,46 @@
-// map.tsx
-import React from 'react';
-import { IonContent, IonPage, IonBackButton, IonButtons, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
-import './map.css';
+// React
+import React from "react";
+
+// Ionic Framework Components
+import {
+	IonContent,
+	IonPage,
+	IonBackButton,
+	IonButtons,
+	IonHeader,
+	IonToolbar,
+	IonTitle,
+} from "@ionic/react";
+
+// Custom Styling
+import "./map.css";
 
 const MapPage: React.FC = () => {
-    // Vous pourriez définir ici le state pour isLoading si nécessaire
-    // const [isLoading, setIsLoading] = useState(true);
-
-    const mapIframe = `
+	const mapIframe = `
     <iframe
-      src="https://www.google.com/maps/d/u/0/embed?mid=1ny_1TOrj65dwzkjIgKCLnqCDwkyzhjA&ehbc=2E312F&z=9"
-      class="map-iframe"
+        src="https://www.google.com/maps/d/u/0/embed?mid=1ny_1TOrj65dwzkjIgKCLnqCDwkyzhjA&ehbc=2E312F&z=9"
+        class="map-iframe"
     ></iframe>`;
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start" className="back_button">
-                        <IonBackButton defaultHref="/" />
-                    </IonButtons>
-                    <IonTitle>Map</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                {/* Supposons que isLoading est faux pour cet exemple et que la carte est toujours affichée */}
-                <div className="map-container" dangerouslySetInnerHTML={{ __html: mapIframe }} />
-            </IonContent>
-        </IonPage>
-    );
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot="start" className="back_button">
+						<IonBackButton defaultHref="/" />
+					</IonButtons>
+					<IonTitle>Map</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				{}
+				<div
+					className="map-container"
+					dangerouslySetInnerHTML={{ __html: mapIframe }}
+				/>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default MapPage;
