160033fd04c28a076835192e80bb69a024f9201a
Maxime <max.ron@outlook.fr>
2023-12-20 16:48:51 +0100

    FEAT[#19]:Add a profile page, that display user info

--- apps/GABOR45/src/pages/profile/Profile.tsx
+++ apps/GABOR45/src/pages/profile/Profile.tsx
@@ -1,84 +1,113 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { IonPage, IonContent, IonButton } from "@ionic/react";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { IonPage, IonContent, IonButton } from '@ionic/react';
 
-import { supabase } from "../../supabaseClient";
-import { User } from "@supabase/supabase-js";
+import { supabase } from '../../supabaseClient';
+import { User } from '@supabase/supabase-js';
 
-import { Utilisateur } from "../../models/User";
-import { getUserInfo } from "../../services/userService";
+import { Utilisateur } from '../../models/User';
+import { getUserInfo } from '../../services/userService';
 
-import LoadingScreen from "../../components/LoadingScreen";
-import { useAuth } from "../../hooks/useAuth";
+import LoadingScreen from '../../components/LoadingScreen';
+import { useAuth } from '../../hooks/useAuth';
 
-import "./Profile.css";
+import './Profile.css';
 
 const Profile: React.FC = () => {
-  const currentUser = useAuth();
-  const [util, setUtil] = useState<Utilisateur | null>(null);
-  const history = useHistory();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+	const currentUser = useAuth();
+	const [util, setUtil] = useState<Utilisateur | null>(null);
+	const history = useHistory();
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (currentUser) {
-        setIsLoading(true);
-        try {
-          const userData = await getUserInfo(currentUser.id);
-          setUtil(userData);
-        } catch (error) {
-          console.error("Failed to fetch user data:", error);
-        } finally {
-          setIsLoading(false);
-        }
-      } else {
-        setIsLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			if (currentUser) {
+				setIsLoading(true);
+				try {
+					const userData = await getUserInfo(currentUser.id);
+					setUtil(userData);
+				} catch (error) {
+					console.error('Failed to fetch user data:', error);
+				} finally {
+					setIsLoading(false);
+				}
+			} else {
+				setIsLoading(false);
+			}
+		};
 
-    fetchData();
-  }, [currentUser]);
+		fetchData();
+	}, [currentUser]);
 
-  const redirectToLogin = () => {
-    history.push("/login");
-  };
+	const redirectToLogin = () => {
+		history.push('/login');
+	};
 
-  const redirectToProfileEdit = () => {
-    if (currentUser) {
-      history.push(`/profile_edit/${currentUser.id}`);
-    } else {
-      redirectToLogin();
-    }
-  };
+	const redirectToProfileEdit = () => {
+		if (currentUser) {
+			history.push(`/profile_edit/${currentUser.id}`);
+		} else {
+			redirectToLogin();
+		}
+	};
 
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Failed to sign out:", error);
-    } else {
-      console.log("Sign out successful");
-      history.push("/login");
-    }
-  };
+	const signOut = async () => {
+		const { error } = await supabase.auth.signOut();
+		if (error) {
+			console.error('Failed to sign out:', error);
+		} else {
+			console.log('Sign out successful');
+			history.replace('/login');
+		}
+	};
 
-  return (
-    <IonPage>
-      {isLoading ? (
-        <LoadingScreen />
-      ) : currentUser ? (
-        <div>
-          Bonjour {currentUser.email}
-          <IonButton onClick={signOut}>Se déconnecter</IonButton>
-          <IonButton onClick={redirectToProfileEdit}>Modifier le Profil</IonButton>
-        </div>
-      ) : (
-        <div>
-          Vous n'êtes pas connecté.
-          <IonButton onClick={redirectToLogin}>Se connecter</IonButton>
-        </div>
-      )}
-    </IonPage>
-  );
+	const baseUrl =
+		'https://sktoqgbcjidoohzeobcz.supabase.co/storage/v1/object/public/avatars/agri/';
+
+	const profileImageUrl = util?.lien_image
+		? `${baseUrl}${util.lien_image}`
+		: '';
+
+	return (
+		<IonPage>
+			<IonContent>
+				{isLoading ? (
+					<LoadingScreen />
+				) : util ? (
+					<div className="content">
+						<div className="profile-image-container">
+							<img
+								src={profileImageUrl}
+								alt="Profil"
+								className="profile-image"
+							/>
+						</div>
+						<div className="information">
+							<h1>Profil</h1>
+							<p>Nom: {util.nom}</p>
+							<p>Prénom: {util.prenom}</p>
+							<p>Email: {util.email}</p>
+							<p>Numéro de téléphone: {util.num_tel}</p>
+							{/* ... Autres informations de profil ... */}
+						</div>
+						<IonButton
+							onClick={redirectToProfileEdit}
+							className="edit-profile"
+						>
+							Modifier le Profil
+						</IonButton>
+						<div className="deconnexion">
+							<IonButton onClick={signOut} expand="block">
+								Se déconnecter
+							</IonButton>
+						</div>
+					</div>
+				) : (
+					<div>Utilisateur non trouvé ou non connecté.</div>
+				)}
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Profile;
