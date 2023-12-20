18a2f392667fffdae781e8a5d49d1a557b88488a
Maxime <max.ron@outlook.fr>
2023-12-20 14:50:08 +0100

    FIX:Fetch user data and UserAuth in Profile page

--- apps/GABOR45/src/pages/profile/Profile.tsx
+++ apps/GABOR45/src/pages/profile/Profile.tsx
@@ -1,100 +1,84 @@
-// React and React Router
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-
-// Ionic Components
 import { IonPage, IonContent, IonButton } from "@ionic/react";
 
-// Supabase Components
 import { supabase } from "../../supabaseClient";
 import { User } from "@supabase/supabase-js";
 
-// Models and Services
 import { Utilisateur } from "../../models/User";
 import { getUserInfo } from "../../services/userService";
 
-// Custom Components
 import LoadingScreen from "../../components/LoadingScreen";
 import { useAuth } from "../../hooks/useAuth";
 
-// Custom Styling
 import "./Profile.css";
 
 const Profile: React.FC = () => {
-	const t = useAuth();
-	const baseUrl =
-		"https://sktoqgbcjidoohzeobcz.supabase.co/storage/v1/object/public/avatars/agri/";
-	const [user, setUser] = useState<User | null>(null);
-	const [util, setUtil] = useState<Utilisateur | null>(null);
-	const history = useHistory();
-	const [isLoading, setIsLoading] = useState<boolean>(true);
+  const currentUser = useAuth();
+  const [util, setUtil] = useState<Utilisateur | null>(null);
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				console.log("ici ca fetch user");
-				if (user != null) {
-					console.log(user);
-					console.log(user.id);
-					console.log("ici ca fetch user");
-					const userData = await getUserInfo(user.id);
-					console.log(userData);
-					setUtil(userData);
-					setIsLoading(false);
-				} else
-					console.log("pas de user donc pas de fetch de user info");
-			} catch (error) {
-				console.error("Failed to fetch data:", error);
-			} finally {
-				//setIsLoading(false);
-			}
-		};
-		setUser(t);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (currentUser) {
+        setIsLoading(true);
+        try {
+          const userData = await getUserInfo(currentUser.id);
+          setUtil(userData);
+        } catch (error) {
+          console.error("Failed to fetch user data:", error);
+        } finally {
+          setIsLoading(false);
+        }
+      } else {
+        setIsLoading(false);
+      }
+    };
 
-		fetchData();
-	}, []);
+    fetchData();
+  }, [currentUser]);
 
-	const redirectToLogin = () => {
-		history.push("/login");
-	};
+  const redirectToLogin = () => {
+    history.push("/login");
+  };
 
-	const redirectToProfileEdit = () => {
-		if (user) {
-			history.push(`/profile_edit/${user.id}`);
-		} else {
-			history.push("/login");
-		}
-	};
+  const redirectToProfileEdit = () => {
+    if (currentUser) {
+      history.push(`/profile_edit/${currentUser.id}`);
+    } else {
+      redirectToLogin();
+    }
+  };
 
-	const signOut = async () => {
-		const { error } = await supabase.auth.signOut();
-		if (error) {
-			console.error(error);
-		} else {
-			console.log("Déconnexion réussie");
-			history.push("/profile");
-		}
-	};
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Failed to sign out:", error);
+    } else {
+      console.log("Sign out successful");
+      history.push("/login");
+    }
+  };
 
-	return (
-		<IonPage>
-			{isLoading ? (
-				<LoadingScreen />
-			) : user ? (
-				<div>
-					Bonjour {user.email}
-					<IonButton onClick={signOut}>Se déconnecter</IonButton>
-					<IonButton onClick={redirectToProfileEdit}>Edit</IonButton>
-				</div>
-			) : (
-				<div>
-					Vous n'êtes pas connecté.
-					<IonButton onClick={redirectToLogin}>
-						Se connecter
-					</IonButton>
-				</div>
-			)}
-		</IonPage>
-	);
+  return (
+    <IonPage>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : currentUser ? (
+        <div>
+          Bonjour {currentUser.email}
+          <IonButton onClick={signOut}>Se déconnecter</IonButton>
+          <IonButton onClick={redirectToProfileEdit}>Modifier le Profil</IonButton>
+        </div>
+      ) : (
+        <div>
+          Vous n'êtes pas connecté.
+          <IonButton onClick={redirectToLogin}>Se connecter</IonButton>
+        </div>
+      )}
+    </IonPage>
+  );
 };
+
 export default Profile;
