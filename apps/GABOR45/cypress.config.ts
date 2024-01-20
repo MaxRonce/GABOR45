4ac285afbec3de6c8ca2e9dbf1a7f8a700937d62
Andres Enrique Ortiz Santa Cruz <104937995+andr3sEnrique@users.noreply.github.com>
2024-01-20 18:19:04 +0100

    Update cypress.config.ts

--- apps/GABOR45/cypress.config.ts
+++ apps/GABOR45/cypress.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
 	baseUrl: 'http://localhost:8100',
 	numTestsKeptInMemory: 0,
 	execTimeout: 60000,
-    video: true,
+	video: true,
 	defaultCommandTimeout: 5000,
-    pageLoadTimeout: 20000,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	pageLoadTimeout: 20000,
+	setupNodeEvents(on, config) {
+	  // implement node event listeners here
+	},
 });
