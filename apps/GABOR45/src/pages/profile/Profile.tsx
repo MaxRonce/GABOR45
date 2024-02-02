e0b3c58c2fa864e6b1ef8f6dccd775f1c75c0bc6
Louis Noel <louisnoel45000@gmail.com>
2024-02-02 09:09:09 +0000

    UPDATE modified profile page display type_produit displayed the uuid now display the name. works asynchronously.

--- apps/GABOR45/src/pages/profile/Profile.tsx
+++ apps/GABOR45/src/pages/profile/Profile.tsx
@@ -7,6 +7,7 @@ import { User } from '@supabase/supabase-js';
 
 import { Utilisateur } from '../../models/User';
 import { getAgriInfo, getUserInfo } from '../../services/userService';
+import { getCategorieFromId } from '../../services/CategorieService';
 
 import LoadingScreen from '../../components/LoadingScreen';
 import { useAuth } from '../../hooks/useAuth';
@@ -19,6 +20,7 @@ const Profile: React.FC = () => {
 	const history = useHistory();
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [farmer_data, setFarmer_data] = useState<any>();
+	const [type_produit, setType_produit] = useState<any>();
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -36,7 +38,18 @@ const Profile: React.FC = () => {
 				setIsLoading(false);
 			}
 			if (currentUser) {
-				setFarmer_data(await getAgriInfo(currentUser?.id || ''));
+				// Get farmer data and wait for it to be set before calling getCategorieFromId
+				await getAgriInfo(currentUser?.id || '')
+					.then(data => {
+						setFarmer_data(data);
+						return data;
+					})
+					.then(async far => {
+						const categorie = await getCategorieFromId(
+							far.type_produit_principal,
+						);
+						setType_produit(categorie && categorie[0]?.name);
+					});
 			}
 		};
 		fetchData();
@@ -89,14 +102,15 @@ const Profile: React.FC = () => {
 		? `${baseUrl}${util.lien_image}`
 		: '';
 
+	const type_pro = async () => {
+		await getCategorieFromId(farmer_data?.type_produit_principal);
+	};
+
 	function infoAgri(): React.ReactNode {
 		return farmer_data ? (
 			<div className="information">
 				<h1>Informations Agriculteur</h1>
-				<p>
-					Type de produit principal:{' '}
-					{farmer_data?.type_produit_principal}
-				</p>
+				<p>Type de produit principal: {type_produit}</p>
 				<p>Description: {farmer_data?.description}</p>
 				<p>Site web: {farmer_data.website}</p>
 				<p>Facebook: {farmer_data.facebook}</p>
@@ -148,7 +162,11 @@ const Profile: React.FC = () => {
 					<div className="content">
 						<h1>Profil</h1>
 						<p>Vous n'êtes pas connecté.</p>
-						<IonButton id='toLogin' onClick={redirectToLogin} expand="block">
+						<IonButton
+							id="toLogin"
+							onClick={redirectToLogin}
+							expand="block"
+						>
 							Se connecter
 						</IonButton>
 					</div>
